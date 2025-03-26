94bc8c9e3db528c42b8f126c0c8c723e2bebca22
Unskilled Wolf <geronimo.xl13@gmail.com>
2025-03-26 13:01:32 +0100

    Containers CSS

--- src/components/TestComponent.tsx
+++ src/components/TestComponent.tsx
@@ -1,3 +1,46 @@
 export default function TestComponent() {
-  return <p>Test Component</p>
+  return (
+    <div className="test-component">
+      <div className="container">
+        <p>Primary Container</p>
+
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam
+          quasi doloribus expedita corrupti. Provident praesentium culpa
+          voluptate debitis ipsam quas suscipit sapiente aliquam nostrum! Minima
+          ex provident aut adipisci repellendus?
+        </p>
+        <button>Button</button>
+        <button className="primary">Primary Button</button>
+        <button className="secondary">Secondary Button</button>
+        <form>
+          <input type="text" name="" id="" />
+          <input type="checkbox" name="" id="" />
+          <input type="button" value="Form Button" />
+          <input type="submit" value="Form Submit" />
+        </form>
+      </div>
+      <div className="container accent">
+        <h2>Accent Container</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam
+          quasi doloribus expedita corrupti. Provident praesentium culpa
+          voluptate debitis ipsam quas suscipit sapiente aliquam nostrum! Minima
+          ex provident aut adipisci repellendus?
+        </p>
+      </div>
+      <div className="container secondary">
+        <h2>Secondary Container</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam
+          quasi doloribus expedita corrupti. Provident praesentium culpa
+          voluptate debitis ipsam quas suscipit sapiente aliquam nostrum! Minima
+          ex provident aut adipisci repellendus?
+        </p>
+      </div>
+    </div>
+  )
 }
